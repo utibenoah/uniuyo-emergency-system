8660c794d588e7de52c1337723aa96415927f97a
Utibe <utibenoaheffiong@gmail.com>
2022-05-28 00:13:36 +0100

    first styling

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let menue=document.querySelector('.togglebar')
-let navlists =document.querySelector('.nav-lists')
+// let menue=document.querySelector('.togglebar')
+// let navlists =document.querySelector('.nav-lists')
 
-// menue.addEventListener('onclick', function () {
+// // menue.addEventListener('onclick', function () {
+// //     navlists.classList.toggle('active')
+// // })
+
+// console.log(navlists)
+// menue.onclick=()=>{
 //     navlists.classList.toggle('active')
-// })
-
-console.log(navlists)
-menue.onclick=()=>{
-    navlists.classList.toggle('active')
-}
+// }
